bdcd49f7c25d9bfa64fac18d96db9d7fcf3823cb
jlnshly <jelaineashleyramos@gmail.com>
2026-06-03 12:11:55 +0800

    Fixed all errors

--- car_program/test_car.py
+++ car_program/test_car.py
@@ -1,16 +1,16 @@
-from car import Car
+from car_class import Car
 
 def main():
-car = Car(2024, "Honda Civic")
-print(f"Test Drive for: {car.get_year_model()}{car.get_make()}")
+    car = Car(2024, "Honda Civic")
+    print(f"Test Drive for: {car.get_year_model()}{car.get_make()}")
 
-print("ACCELERATING:")
-car.accelerate()
-print(f"Current Speed: {car.get_speed()}km/h")
+    print("ACCELERATING:")
+    car.accelerate()
+    print(f"Current Speed: {car.get_speed()}km/h")
 
-print("BRAKING:")
-car.brake()
-print(f"Current Speed: {car.get_speed()}km/h")
+    print("BRAKING:")
+    car.brake()
+    print(f"Current Speed: {car.get_speed()}km/h")
 
 if __name__ == "__main__":
-main()
+    main()
