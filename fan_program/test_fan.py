f2686288110e124657f4746c27c3ac8e85068407
jlnshly <jelaineashleyramos@gmail.com>
2026-06-03 11:45:50 +0800

    Second object done

--- fan_program/test_fan.py
+++ fan_program/test_fan.py
@@ -2,3 +2,5 @@ from fan_class import Fan
 
 def main():
     fan_one = Fan(speed=Fan.FAST, radius=10.0, color="yellow", on=True)
+    fan_two = Fan(speed=Fan.MEDIUM, radius=5.0, color="blue", on=False)
+
