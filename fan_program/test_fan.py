4bfe0ef5f6df3b3b26affff27ec3b5ea186d2d10
jlnshly <jelaineashleyramos@gmail.com>
2026-06-03 11:54:11 +0800

    Changed 'demo' to status instead

--- fan_program/test_fan.py
+++ fan_program/test_fan.py
@@ -4,7 +4,7 @@ def main():
     fan_one = Fan(speed=Fan.FAST, radius=10.0, color="yellow", on=True)
     fan_two = Fan(speed=Fan.MEDIUM, radius=5.0, color="blue", on=False)
 
-    print("Fan Demo")
+    print("Fan Status")
     print(fan_one)
     print(fan_two)
 
