cab1f089452df996cf5b79a446c5a148983a5c2d
jlnshly <jelaineashleyramos@gmail.com>
2026-06-03 12:09:09 +0800

    Created test drive functions and fixed indentation

--- car_program/test_car.py
+++ car_program/test_car.py
@@ -1,4 +1,16 @@
 from car import Car
 
 def main():
-    car = Car()
+car = Car(2024, "Honda Civic")
+print(f"Test Drive for: {car.get_year_model()}{car.get_make()}")
+
+print("ACCELERATING:")
+car.accelerate()
+print(f"Current Speed: {car.get_speed()}km/h")
+
+print("BRAKING:")
+car.brake()
+print(f"Current Speed: {car.get_speed()}km/h")
+
+if __name__ == "__main__":
+main()
