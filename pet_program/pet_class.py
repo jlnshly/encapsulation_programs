f3dab00d9c7c346fbe10c25aa6e7fb099264b8e1
jlnshly <jelaineashleyramos@gmail.com>
2026-06-03 12:25:27 +0800

    Done creating the attributes

--- pet_program/pet_class.py
+++ pet_program/pet_class.py
@@ -6,3 +6,18 @@ class Pet:
 
     def set_name(self, name):
         self.__name = name.strip().title()
+
+    def set_animal_type(self, animal_type):
+        self.__animal_type = animal_type.strip().title()
+
+    def set_age(self, age):
+        self.__age = age
+
+    def get_name(self):
+        return self.__name
+
+    def get_animal_type(self):
+        return self.__animal_type
+
+    def get_age(self):
+        return self.__age
