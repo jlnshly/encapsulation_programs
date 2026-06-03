968d2156d485db54bfce867ea921ddcdfc284823
jlnshly <jelaineashleyramos@gmail.com>
2026-06-03 12:28:06 +0800

    Created user input messages

--- pet_program/test_pet.py
+++ pet_program/test_pet.py
@@ -1,6 +1,6 @@
 from pet_class import Pet
 
 def main():
-    pet_name = input()
-    pet_type = input()
-    pet_age = input()
+    pet_name = input("Enter pet name: ")
+    pet_type = input("Enter pet type: ")
+    pet_age = input("Enter pet age: ")
