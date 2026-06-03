8a9785f51e61f6b5707d1ff774d6d4eb74b71f2f
jlnshly <jelaineashleyramos@gmail.com>
2026-06-03 12:30:55 +0800

    Final touches

--- pet_program/test_pet.py
+++ pet_program/test_pet.py
@@ -6,3 +6,16 @@ def main():
     pet_age = input("Enter pet age: ")
 
     my_pet = Pet()
+    my_pet.set_name = pet_name
+    my_pet.set_type = pet_type
+    my_pet.set_age(pet_age)
+
+    print("\nRegistering pet into the system...")
+    print("\nHere's your registration details:")
+    print(f"Name: {my_pet.get_name()}")
+    print(f"Type: {my_pet.get_type()}")
+    print(f"Age: {my_pet.get_age()}")
+
+if __name__ == "__main__":
+    main()
+
