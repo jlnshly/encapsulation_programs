5be968379cad7853834b780299ed8f15a9fa5008
jlnshly <jelaineashleyramos@gmail.com>
2026-06-03 12:00:32 +0800

    Adding other methods

--- car_program/car_class.py
+++ car_program/car_class.py
@@ -3,3 +3,9 @@ class Car:
         self.__year_model = year_model
         self.__make = make
         self.__speed = 0
+
+    def accelerate(self):
+        self.__speed += 5
+
+    def brake(self):
+        self.__speed -= 5
